1e6577fd19e6142680d95d61d8e607072c85509e
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-14 13:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+  let str1 = s1.toLowerCase();
+  let str2 = s2.toLowerCase();
+
+	return str1.indexOf(str2);
 }
 
 // Please do not change the code below
